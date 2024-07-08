217aa4ba607edd7da2b1cf6bdf0123d0e5db56df
Moealsir <mohamedwdalsir@gmail.com>
2024-07-08 23:45:56 +0300

    1-concurrent_coroutines.py

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Task 3's module.'''
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+  '''Executes wait_random n times.'''
+  return asyncio.create_task(wait_random(max_delay))
